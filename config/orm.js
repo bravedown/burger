066a304448269838f5e22e551fd9f15f0e114fe3
David <davidbrown3142@gmail.com>
2020-11-08 18:35:23 -0500

    insertOne and updateOne working

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,21 @@
 const connection = require("./connection");
 module.exports = {
-    selectAll(cb) {
-        connection.query("SELECT * from burgers", [], (err, result) => {
+    selectAll(table, cb) {
+        connection.query("SELECT * from ??", [table], (err, result) => {
             if (err)
                 throw err;
             cb(result);
         });
     },
-    insertOne() {
-
+    insertOne(tableName, colName, cellName) {
+        connection.query(`INSERT INTO ?? (??) VALUE (?)`, [tableName, colName, cellName], (err, result) => {
+            if (err)
+                throw err;
+        });
     },
-    updateOne() {
-
+    updateOne(table, col, val, id) {
+        connection.query(`UPDATE ?? SET ?? = ? WHERE id = ?`, [table, col, val, id], function(err, result) {
+            if (err) throw err;
+        });
     },
 };
