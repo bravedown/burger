e19214e392d5e93e33b89dcd7325f538e01731cb
David <davidbrown3142@gmail.com>
2020-11-08 17:38:20 -0500

    setup

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name) VALUE ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUE ("Hamburger");
+INSERT INTO burgers (burger_name) VALUE ("Slamburger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Baconator", 1);
+
