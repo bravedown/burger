015348f795794c25d72ea0c1d7e7415830a9f839
David <davidbrown3142@gmail.com>
2020-11-08 18:45:06 -0500

    updated updateOne

--- config/orm.js
+++ config/orm.js
@@ -2,19 +2,17 @@ const connection = require("./connection");
 module.exports = {
     selectAll(table, cb) {
         connection.query("SELECT * from ??", [table], (err, result) => {
-            if (err)
-                throw err;
+            if (err) throw err;
             cb(result);
         });
     },
     insertOne(tableName, colName, cellName) {
         connection.query(`INSERT INTO ?? (??) VALUE (?)`, [tableName, colName, cellName], (err, result) => {
-            if (err)
-                throw err;
+            if (err) throw err;
         });
     },
-    updateOne(table, col, val, id) {
-        connection.query(`UPDATE ?? SET ?? = ? WHERE id = ?`, [table, col, val, id], function(err, result) {
+    updateOne(table, updatedCol, referenceCol, newVal, referenceVal) {
+        connection.query(`UPDATE ?? SET ?? = ? WHERE ?? = ?`, [table, updatedCol, newVal, referenceCol, referenceVal], function(err, result) {
             if (err) throw err;
         });
     },
