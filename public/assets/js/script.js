0c3f781ab90808215a55700da5f8f18f8ae60f30
David <davidbrown3142@gmail.com>
2020-11-09 15:01:41 -0500

    small refactor

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,24 +1,24 @@
 $(function() {
     $(".change-burger").on("click", function(event) {
-      var id = $(this).data("id");
-      var devouredState = {
-        devoured: !$(this).data("devoured")
-      };
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: devouredState
-      }).then(() => location.reload());
+        var id = $(this).data("id");
+        var devouredState = {
+            devoured: !$(this).data("devoured")
+        };
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(() => location.reload());
     });
   
     $(".create-form").on("submit", function(event) {
-      event.preventDefault();
-      var newBurger = {
-        burgerName: $("#new-burger-name").val().trim()
-      };
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(() => location.reload());
+        event.preventDefault();
+        var newBurger = {
+            burgerName: $("#new-burger-name").val().trim()
+        };
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(() => location.reload());
     });
-  });
+});
   
