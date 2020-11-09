3da648061bc5ef567bac3c8997399277f9cd5da7
David <davidbrown3142@gmail.com>
2020-11-08 19:44:55 -0500

    all burger methods working

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,13 @@
 const orm = require("../config/orm");
 
 module.exports = {
-
+    getBurgers() {
+        return orm.selectAll('burgers');
+    },
+    addBurger(name) {
+        orm.insertOne('burgers', 'burger_name', name)
+    },
+    devourBurger(name) {
+        orm.updateOne('burgers', 'devoured', 'burger_name', 1, name);
+    }
 };
