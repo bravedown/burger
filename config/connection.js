94ac398a7ff6c536f75bea1aaf709525e5ce8055
David <davidbrown3142@gmail.com>
2021-01-01 19:47:29 -0500

    jawsDB for database

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 module.exports = mysql.createConnection({
-  host: "localhost",
+  host: "dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "test_user",
-  password: "password",
-  database: "burgers_db"
+  user: "rsr67wqd2eafpnsc",
+  password: "drquhlkumpr4cjfz",
+  database: "hzllmpe8eet5k0fr"
 });
 
